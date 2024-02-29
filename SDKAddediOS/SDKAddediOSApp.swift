cea81b95f11f62231b241400cd44eed0c8244220
Dimuthu Lakshan <dimuthu_l@epiclanka.net>
2024-02-29 11:04:59 +0530

    Initial Commit

---
+++ SDKAddediOS/SDKAddediOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDKAddediOSApp.swift
+//  SDKAddediOS
+//
+//  Created by Dimuthu Lakshan on 2024-02-29.
+//
+
+import SwiftUI
+
+@main
+struct SDKAddediOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
